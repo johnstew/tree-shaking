98dff1c5fa48cb325c3c93f22b6a1a1276ca0eb5
jstewart <johnathan.stewart@mlb.com>
2019-03-05 16:32:58 -0500

    Updated Basic

---
+++ README.md
@@ -0,0 +1,13 @@
+# tree-shaking
+
+This repo basically follows this webpack doc: [tree-shaking](https://webpack.js.org/guides/tree-shaking/) but the goal of this is to explore at what points this breaks down.
+
+You can hop to different points in the code by checking out the different tags. They increase in complexity of the application.
+
+This stuff is kind of hard to detail in a readme so you will have to take a look at the dist folder and each build to see the output.
+
+I attempted to beautify it as much as I could.
+
+## Step 1
+
+This is basically the tutorial still. So I have the `math.js` that exports two functions and I only use one of them as part of my app. These are named exports and pretty easy for webpack to identify.
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,59 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = {
-  devtool: 'none',
-  entry: {
-    index: './src/index.js'
+module.exports = [
+  {
+    devtool: 'none',
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      filename: 'dev.bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    optimization: {
+      usedExports: true
+    }
   },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  {
+    devtool: 'none',
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      filename: 'prod.bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    optimization: {
+      usedExports: true
+    }
   },
-  mode: 'development',
-  optimization: {
-    usedExports: true
+  {
+    devtool: 'none',
+    entry: {
+      index: './src/index.js'
+    },
+    output: {
+      filename: 'prod-readable.bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production',
+    optimization: {
+      usedExports: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            mangle: false,
+            compress: {
+              inline: false,
+              keep_classnames: true,
+              keep_fnames: true
+            }
+          }
+        })
+      ]
+    }
   }
-};
+];
